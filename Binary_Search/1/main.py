d271c0c5852726f9b88b43f2763110990b356c28
salehabdulla <salehabdullaict@gmail.com>
2026-03-17 04:07:16 +0300

    implementing binary search

---
+++ Binary_Search/1/main.py
@@ -0,0 +1,23 @@
+from typing import List
+
+
+arr = [100, 101, 102, 103, 104, 105, 106, 107, 108, 109,110]
+
+
+def binary_search(arr: List[int], target: int) -> int:
+
+    left, right = 0, len(arr) - 1
+
+    while left <= right:
+        mid = (left + right) // 2
+        
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+
+res = binary_search(arr, 106)
+print(res)
+
